cd96542a60328515399a903be14a64a090949522
Anshul <anshul.treats@yahoo.com>
2021-06-04 02:19:11 -0400

    Added algorithm for generating 7 random cards

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.exercise1;
 
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,8 +22,13 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            Random rand = new Random();
+            int cardValue = rand.nextInt(13) + 1;            
+            int suitValue = rand.nextInt(4);
+
+            c.setValue(cardValue);
+            c.setSuit(Card.SUITS[suitValue]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
