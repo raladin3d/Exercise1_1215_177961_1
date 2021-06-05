02c115dbd532610280ddb79b9edc8b00af7769c9
alid111 <84792974+alid111@users.noreply.github.com>
2021-06-05 16:50:01 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -5,28 +5,59 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.exercise1;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modified by Danish Ali, Student ID: 991628757
  */
+
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Card[] magicHand = produceMagicHand();
+        Card selectedCard = askSelectedCard();
+        displayResult(magicHand, selectedCard);
+    }
+    
+    public static Card[] produceMagicHand(){
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
+        for (int i=0; i<magicHand.length; i++){
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            Random rand = new Random();
+            int cardValue = rand.nextInt(13) + 1;            
+            int suitValue = rand.nextInt(4);
+
+            c.setValue(cardValue);
+            c.setSuit(Card.SUITS[suitValue]);
+            
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        return magicHand;
+    }
+    
+    public static Card askSelectedCard(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the card number (1-13): ");
+        int selectedCardNumber = input.nextInt();
+
+        System.out.print("Enter the card suit (0: Spades, 1: Diamonds, "
+                + "2: Hearts, 3:Clubs): ");
+        int selectedCardSuit = input.nextInt();
+
+        Card selectedCard = new Card();
+        selectedCard.setValue(selectedCardNumber);
+        selectedCard.setSuit(Card.SUITS[selectedCardSuit]);
+        
+        return selectedCard;
     }
     
 }
