52548a5afd9d541c0120e68acccc88c9a6fcb330
su11ya <su11@sheridancollege.ca>
2021-06-06 13:00:26 -0400

    First Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -29,34 +29,34 @@ public class CardTrick {
             magicHand [i] = c;
         }
         
-        Card userCard = new Card();
-        int value;
-        String suit;
-        boolean found = false;
-        
-        //insert code to ask the user for Card value and suit, create their card
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the card value(1-13) : ");
-        value = input.nextInt();
-        System.out.println("Enter the card suit (Hearts, Diamonds, Spades, Clubs): ");
-        suit = input.next();
-        userCard.setValue(value);
-        userCard.setSuit(suit);
-        
-        // and search magicHand here
-        for(int i=0; i<magicHand.length; i++){
-            if(magicHand[i].getValue()==userCard.getValue() && magicHand[i].getSuit()==userCard.getSuit()){
-                found = true;
-                break;
-            }
-        }
-        
-        //Then report the result here
-        if(found){
-            System.out.println("User's card match the magicHand");
-        } else {
-            System.out.println("User's card not match the magicHand");
-        }
+//        Card userCard = new Card();
+//        int value;
+//        String suit;
+//        boolean found = false;
+//        
+//        //insert code to ask the user for Card value and suit, create their card
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the card value(1-13) : ");
+//        value = input.nextInt();
+//        System.out.println("Enter the card suit (Hearts, Diamonds, Spades, Clubs): ");
+//        suit = input.next();
+//        userCard.setValue(value);
+//        userCard.setSuit(suit);
+//        
+//        // and search magicHand here
+//        for(int i=0; i<magicHand.length; i++){
+//            if(magicHand[i].getValue()==userCard.getValue() && magicHand[i].getSuit()==userCard.getSuit()){
+//                found = true;
+//                break;
+//            }
+//        }
+//        
+//        //Then report the result here
+//        if(found){
+//            System.out.println("User's card match the magicHand");
+//        } else {
+//            System.out.println("User's card not match the magicHand");
+//        }
     }
     
 }
