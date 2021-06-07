d4413d8d36f4b1f9be1f36f6061c679d699ce9cc
asidhu3 <46944909+asidhu3@users.noreply.github.com>
2021-06-06 22:32:18 -0400

    Reverted to original source code

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
@@ -7,12 +7,12 @@ package ca.sheridancollege.week3.softwarefundamentals.exercise1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author asidhu3
+ * @author dancye
  */
 public class Card {
 
@@ -23,9 +23,6 @@ public class Card {
     /**
      * @return the suit
      */
-   
-
-   
     public String getSuit() {
         return suit;
     }
