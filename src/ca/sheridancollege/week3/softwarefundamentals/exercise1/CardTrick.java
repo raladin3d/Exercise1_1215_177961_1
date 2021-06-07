27adea50859cc459bbd81f95ae01bef689be0de2
Lenovo <Lenovo@192.168.2.67>
2021-06-07 02:36:49 -0400

    First commit

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -11,22 +11,42 @@ package ca.sheridancollege.week3.softwarefundamentals.exercise1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        boolean result = false;
         
+        Scanner input = new Scanner(System.in);
+        
+        Card c = new Card();
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
+            magicHand[i]= new Card();
             //c.setValue(insert call to random number generator here)
+            int random_num = (int)Math.floor(Math.random()*(13-1+1)+1);
+            magicHand[i].setValue(random_num);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int random_suit = (int)Math.floor(Math.random()*(3-0+1)+0);
+            magicHand[i].setSuit(Card.SUITS[random_suit]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.print("Please enter the Card Value ");
+        int u_value = input.nextInt();
+        System.out.println("Please 0.Hearts 1.Diamonds 2.Spades 3.Clubs ");
+        int u_suit = input.nextInt();
+        
+        c.setValue(u_value);
+        c.setSuit(c.SUITS[u_suit]);
+        
+        
         // and search magicHand here
+        
         //Then report the result here
+        
     }
     
 }
