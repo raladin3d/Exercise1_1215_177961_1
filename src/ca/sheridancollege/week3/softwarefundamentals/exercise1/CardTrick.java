82571aa53522321ef4a2c5bb73be6f79317597c1
1coin24k <fan3@sheridancollege.ca>
2021-06-09 02:43:10 +0800

    First commit

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -2,9 +2,12 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * modifier: Li Fan
+ * Student number: 991630397
  */
 package ca.sheridancollege.week3.softwarefundamentals.exercise1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -21,10 +24,20 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(new Random(). nextInt (13));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit (Card.SUITS[new Random().nextInt (13)]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner input = new Scanner (System.in);
+        System.out.print ("Card value");
+        int Cardvalue = input.nextInt();
+        
+        System.out.print ("Card suit [Hearts, Diamonds, Spades, Clubs]");
+        
+        String Cardsuit = input.next();
         // and search magicHand here
         //Then report the result here
     }
