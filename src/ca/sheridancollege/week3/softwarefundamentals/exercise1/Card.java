9a99a9dd23619149765280ca4c85183601a1c241
Alhabbal-M <84793510+Alhabbal-M@users.noreply.github.com>
2021-06-08 16:15:34 -0400

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
@@ -1,18 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * SYST17796 Fundamentals of Software Design 
+ * Mohamed Alhabbal
+ * Student Number: 991521294
+ * Date: 2021/06/08
  */
+
 package ca.sheridancollege.week3.softwarefundamentals.exercise1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
  * @author dancye
+ * @author Mohamed Alhabbal
  */
 public class Card {
 
@@ -51,3 +52,4 @@ public class Card {
    
     
 }
+
